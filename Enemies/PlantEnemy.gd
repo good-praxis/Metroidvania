15d320d7cf019b6a9dea1ce698c9f943d888c287
Praxis <hello@vulpinecat.com>
2020-05-23 16:12:58 +0200

    add sleep state to plant

--- Enemies/PlantEnemy.gd
+++ Enemies/PlantEnemy.gd
@@ -4,9 +4,20 @@ const EnemyBullet = preload("res://Enemies/EnemyBullet.tscn")
 
 export(int) var BULLET_SPEED: = 30
 export(float) var SPREAD: = 30.0
+export(bool) var sleep: = false
 
 onready var fireDirection = $FireDirection
 onready var bulletSpawnPoint = $BulletSpawnPoint
+onready var anim = $AnimationPlayer
+
+func _ready():
+	if sleep:
+		anim.stop()
+		
+func _on_Hurtbox_hit(damage):
+	sleep = false
+	anim.play("Animate")
+	._on_Hurtbox_hit(damage)
 
 func fire_bullet():
 	var bullet = Utils.instance_scene_on_main(EnemyBullet, bulletSpawnPoint.global_position)
